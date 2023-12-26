fd03a3bb8c264e67283d2aae14eb450b8a7bcc05
james kiongo <jameskiongo@jamess-MacBook-Air.local>
2023-12-26 18:53:41 +0300

    django

--- storeront/playground/urls.py
+++ storeront/playground/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 #url conf
 urlpatterns = [
-    path('hello/', views.say_hello)
+    path('hello/', views.say_hello),
+    path('brian/', views.brian, name = "brian"),
+    path('david/', views.david, name = "david"),
+    path("<str:name>", views.greet, name = "greet")
 ]
--- storeront/playground/views.py
+++ storeront/playground/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 # request handler
+def index(request):
+    return render(request, "playground/hello.html")
 def say_hello(request):
     return render(request, 'hello.html', {'name': 'mosh'})
+def brian(request):
+    return HttpResponse("Hello, brian")
+def david(request):
+    return HttpResponse("hello, david")
+def greet(request, name):
+    return HttpResponse(f"hello, {name.capitalize()}")
